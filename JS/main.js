17301211c324f226186cc7bd6f4d0ad213211bc6
Majo <morantesd1@miumg.edu.gt>
2026-05-16 23:02:06 -0600

    first commit

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-olvido").addEventListener("click", function() {
+    document.getElementById("modal-bg").style.display = "flex";
+});
+
+// Cerrar modal
+document.getElementById("btn-cerrar").addEventListener("click", function() {
+    document.getElementById("modal-bg").style.display = "none";
+});
